2e11633e8b3aebab82fc3f25ae8ad9bd19260b61
Ditto <yanzhiking@126.com>
2022-12-09 15:08:57 +0800

    去掉 initGlobalState

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { registerMicroApps, start, initGlobalState } from 'qiankun'
+import { registerMicroApps, start } from 'qiankun'
 import apps from './micro-app'
 import ElementUI from 'element-ui'
 import { Loading } from 'element-ui'
-// import Loading from '@@/micro-common/components/Loading'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { size: 'small' })
-// ¸²¸ÇelementµÄloading
+
 Vue.use(Loading)
 new Vue({
   router,
